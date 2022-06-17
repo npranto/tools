b26b429acf95606e42417c66847ce7eb9191186f
Nazmuz Pranto <npranto@gmail.com>
2022-06-16 22:07:33 -0400

    still adding setup-husky command

--- src/setupHusky.js
+++ src/setupHusky.js
@@ -1,5 +1,6 @@
 const chalk = require('chalk');
 const path = require('path');
+const fs = require('fs');
 const process = require('process');
 const { exec } = require('child_process');
 
@@ -15,22 +16,27 @@ const { exec } = require('child_process');
 // 	// do something
 // }
 
+const doesDirectoryExist = (dir) => {
+	try {
+		return fs.existsSync(dir);
+	} catch (e) {
+		return false;
+	}
+};
+
 const executeCommand = (command) => {
 	return new Promise((resolve) => {
 		exec(`${command}`, (error, stdout, stderr) => {
 			if (error) {
-				// console.log(`error: ${error.message}`);
 				resolve({
 					success: false,
 				});
 			}
 			if (stderr) {
-				// console.log(`stderr: ${stderr}`);
 				resolve({
 					success: false,
 				});
 			}
-			// console.log(`stdout: ${stdout}`);
 			resolve({
 				success: true,
 				output: stdout,
@@ -39,13 +45,72 @@ const executeCommand = (command) => {
 	});
 };
 
+const getPackageJSON = () => {
+	const filePath = path.join(process.cwd(), 'package.json');
+	try {
+		return JSON.parse(fs.readFileSync(filePath, 'utf8'));
+	} catch (e) {
+		throw e;
+	}
+};
+
+const isPackageInstalled = (package = '', packageJSON = {}) => {
+	const { dependencies = {}, devDependencies = {} } = packageJSON;
+	return dependencies[package] || devDependencies[package];
+};
+
+const installPackage = async (package, version, asDevDependency) => {
+	const packageJSON = getPackageJSON();
+	if (isPackageInstalled(package, packageJSON)) {
+		console.log(
+			chalk.green.bold(
+				`Looks like (${package}) is installed already, skipping installation`
+			)
+		);
+		return;
+	}
+	console.log(
+		chalk.gray(
+			`Installing version (${version ? version : 'latest'}) of (${package})...`
+		)
+	);
+	const name = version ? `${package}@${version}` : `${package}`;
+	const devDependencyFlag = asDevDependency ? '-D' : '';
+	await executeCommand(`npm install ${name} ${devDependencyFlag}`);
+	console.log(chalk.green.bold('DONE'));
+};
+
+const isGitInitialized = () => {
+	return doesDirectoryExist(path.join(process.cwd(), '.git'));
+};
+
+const initializeGit = async () => {
+	if (isGitInitialized()) {
+		console.log(
+			chalk.green.bold(`Looks like \`git\` is already initialized, skipping`)
+		);
+	} else {
+		console.log(chalk.gray('Initializing git...'));
+		await executeCommand('git init');
+		console.log(chalk.green.bold('DONE'));
+	}
+};
+
+const addAllGitHook = () => {
+	// TODO:
+};
+
 const setupHusky = async () => {
 	const currentDir = process.cwd();
 	console.log({ currentDir });
 
-	const { success, output } = await executeCommand('ls -la');
+	await installPackage('husky', null, true);
 
-	console.log("after executeCommand('ls -la')...", { success, output });
+	await initializeGit();
+
+	const { success } = executeCommand('npx husky install');
+
+	await addAllGitHook();
 
 	return;
 
