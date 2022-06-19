b3b7052c350cd114d52cb96b1e96a15fc6b503ba
Nazmuz Pranto <npranto@gmail.com>
2022-06-19 17:22:38 -0400

    adds spacing before and after TODOs

--- src/setupPrettier.js
+++ src/setupPrettier.js
@@ -2,11 +2,13 @@ const chalk = require('chalk');
 
 const setupPrettier = async () => {
 	console.log(chalk.green.bold('👉 Setting up prettier...'));
+
 	// TODO:
 	// - install prettier package as a devDependency
 	// - adds \`.prettierrc\` file w/ default configurations
 	// - adds \`.prettierrc\` with default ignore files
 	// - adds \`format\` npm script inside package.json file to auto formats all the files w/in your project
+
 	console.log(chalk.green.bold('✅ Success! Prettier is setup now!'));
 };
 
