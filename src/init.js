8ba436a2e075e34d68cdfe8416c6fcc59d665d5d
Nazmuz Pranto <npranto@gmail.com>
2022-06-19 17:21:50 -0400

    adding setup-prettier command

--- src/init.js
+++ src/init.js
@@ -1,6 +1,7 @@
 const chalk = require('chalk');
 const { program } = require('commander');
 const setupHusky = require('./setupHusky');
+const setupPrettier = require('./setupPrettier');
 
 program
 	.command('setup-husky')
@@ -13,5 +14,17 @@ program
 		`
 	)
 	.action(setupHusky);
+
+program
+	.command('setup-prettier')
+	.description(
+		`
+		 Installs prettier package as a devDependency, adds \`.prettierrc\` file w/ default configurations, adds \`.prettierrc\` with default ignore files, adds \`format\` npm script inside package.json file to auto formats all the files w/in your project
+
+		 ${chalk.blue.bold.underline('BEFORE YOU RUN')}:
+		 - only run this command from the root of your project, otherwise husky may not setup properly
+		`
+	)
+	.action(setupPrettier);
 
 program.parse();
