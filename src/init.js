5b737b01012c3ac00c69a6cef33c03266b3fc7ca
Nazmuz Pranto <npranto@gmail.com>
2022-06-20 15:26:55 -0400

    adds new flag - '-C' or '--custom' to create a customized prettier config

--- src/init.js
+++ src/init.js
@@ -28,9 +28,15 @@ program
 		 - adds \`.prettierignore\` with default ignore files
 		 - adds \`format\` npm script inside package.json file to auto formats all the files w/in your project
 
+		 -C, --custom (to setup your own prettier configurations w/ a set of questionnaires)
+
 		 ${chalk.blue.bold.underline('BEFORE YOU RUN')}:
 		 - only run this command from the root of your project, otherwise husky may not setup properly
 		`,
+	)
+	.option(
+		'-C, --custom',
+		'setup your own prettier configurations w/ a set of questionnaires',
 	)
 	.action(setupPrettier);
 
